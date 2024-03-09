d24432db1574ee5994f5face8a3dd60ab9ba73cd
sean berrios <berrios96sean@gmail.com>
2024-03-08 17:30:55 -0700

    update sim to work with proxy code

--- sim/compile.tcl
+++ sim/compile.tcl
@@ -10,19 +10,26 @@ vlog -suppress 12110 axis_traffic_gen.v
 
 # compile shared files 
 vlog -suppress 12110 ../src/shared_verilog/noc_router_adapter_block.v 
-vlog -suppress 12110 ../src/shared_verilog/simple_collector.v
-vlog -suppress 12110 ../src/shared_verilog/simple_dispatcher.v
-# compile MVM that includes IP blocks
-vlog -suppress 12110 ../src/shared_verilog/simple_mvm_new.v  
+
+### Simple Design ###
+# vlog -suppress 12110 ../src/shared_verilog/simple_collector.v
+# vlog -suppress 12110 ../src/shared_verilog/simple_dispatcher.v
+# vlog -suppress 12110 ../src/shared_verilog/simple_mvm_new.v  
+
+### Stratix Design ###
+# vlog -suppress 12110 ../src/shared_verilog/hard_block_include.v 
+vlog -suppress 12110 ../src/shared_verilog/collector-proxy-stratixiv.v
+vlog -suppress 12110 ../src/shared_verilog/dispatcher-proxy-stratixiv.v
+vlog -suppress 12110 ../src/shared_verilog/mvm-proxy-stratixiv.v 
 
 # compile DSP Instance 
-vlog -suppress 12110 ../src/int_sop_2/altera_c10gx_native_fixed_point_dsp_1910/synth/int_sop_2_altera_c10gx_native_fixed_point_dsp_1910_6lgytvi.v
-vlog -suppress 12110 ../src/int_sop_2/synth/int_sop_2.v
+# vlog -suppress 12110 ../src/int_sop_2/altera_c10gx_native_fixed_point_dsp_1910/synth/int_sop_2_altera_c10gx_native_fixed_point_dsp_1910_6lgytvi.v
+# vlog -suppress 12110 ../src/int_sop_2/synth/int_sop_2.v
 
 # compile single port ram 
-vlog -suppress 12110 ../src/single_port_ram/ram_1port_2011/synth/single_port_ram_ram_1port_2011_otpmvnq.v
-vlog -suppress 12110 ../src/single_port_ram/synth/single_port_ram.v
+# vlog -suppress 12110 ../src/single_port_ram/ram_1port_2011/synth/single_port_ram_ram_1port_2011_otpmvnq.v
+# vlog -suppress 12110 ../src/single_port_ram/synth/single_port_ram.v
 
 # compile MLP top 
-vlog -suppress 12110 -L altera_ver -L lpm_ver -L sgate_ver -L altera_mf_ver -L altera_lnsim_ver -L cyclone10gx_ver ../src/mvm1/mlp_1.v 
+vlog -suppress 12110 ../src/mvm1/mlp_1.v 
 
--- sim/simg.tcl
+++ sim/simg.tcl
@@ -5,7 +5,7 @@ source compile.tcl > compile_log.log
 
 vsim -c -do "exit"  
 
-vsim -voptargs=+acc -L altera_ver -L lpm_ver -L sgate_ver -L altera_mf -L altera_mf_ver -L altera_lnsim_ver -L cyclone10gx_ver -L work -suppress 14408 -suppress 16154 -suppress 12110 work.mvm1_tb -do "do wave.do; run -all"
+vsim -voptargs=+acc -L altera_ver -L lpm_ver -L sgate_ver -L altera_mf_ver -L altera_lnsim_ver -L cyclone10gx_ver -L work -suppress 14408 -suppress 16154 -suppress 12110 work.mvm1_tb -do "do wave.do; run -all"
 
 # # add waveform signals 
 # do ./wave.do
